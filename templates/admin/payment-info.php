6019a66990bea0a7e3dc4ae00fc119f59e848a5d
Ozerk Usgurlu <ozerk.usgurlu@paxful.com>
2021-06-07 15:42:52 +0300

    Support link updated, code style updated

--- templates/admin/payment-info.php
+++ templates/admin/payment-info.php
@@ -7,8 +7,8 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 <div>
-	<strong><?php esc_html_e( 'Payment Info', 'paxful-payments' ); ?></strong>
-	<br/>
-	<strong><?php esc_html_e( 'Track ID', 'paxful-payments' ); ?>:</strong> <?php echo esc_html( $track_id ); ?>
-	<br/>
+    <strong><?php esc_html_e( 'Payment Info', 'paxful-payments' ); ?></strong>
+    <br/>
+    <strong><?php esc_html_e( 'Track ID', 'paxful-payments' ); ?>:</strong> <?php echo esc_html( $track_id ); ?>
+    <br/>
 </div>
